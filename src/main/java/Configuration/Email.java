6a73880086f56e6ba1d287063ab451c117533351
Robin Duda <robin.duda@idainfront.se>
2017-10-07 00:45:45 +0200

    Add missing email configuration class and update vertx version.

---
+++ src/main/java/Configuration/Email.java
@@ -0,0 +1,11 @@
+package Configuration;
+
+/**
+ * Configuration for email notifications.
+ */
+public class Email {
+    public static final String HOST = "?";
+    public static final String USERNAME = "?";
+    public static final String PASSWORD = "?";
+    public static final int PORT = 21;
+}
